e70382040bd6f85e4efaa4524cdea05e6ed3516d
Maria <mliberov@gmail.com>
2015-03-10 16:12:37 -0400

    Work on projects model, view, and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.create(project_name: 'Snuffleuppagus', allowed_hours: '100')
