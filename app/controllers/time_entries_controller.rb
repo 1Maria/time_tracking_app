51e89c07f20c891e47842ae01cce880b41c198d4
Maria <mliberov@gmail.com>
2015-03-11 10:37:22 -0400

    Work on time entries controller and views

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,22 +1,61 @@
 class TimeEntriesController < ApplicationController
+  before_action :set_time_entry, only: [:show, :edit, :update, :destroy]
+
   def index
+    @time_entries = TimeEntry.all
   end
 
   def show
   end
 
   def new
+    @time_entry = TimeEntry.new
   end
 
   def edit
   end
 
   def create
+    @time_entry = TimeEntry.new(time_entry_params)
+
+    respond_to do |format|
+      if @time_entry.save
+        format.html { redirect_to @time_entry, notice: 'Time entry was successfully saved.' }
+        format.json { render :show, status: :created, location: @time_entry }
+      else
+        format.html { render :new }
+        format.json { render json: @time_entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @time_entry.update(time_entry_params)
+        format.html { redirect_to @time_entry, notice: 'Time entry was sucessfully updated.' }
+        format.json { render :show, status: :ok, location: @time_entry }
+      else
+        format.html { render :edit }
+        format.json { render json: @time_entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @time_entry.destroy
+    respond_to do |format|
+      format.html { redirect_to time_entries_url, notice: 'Time entry was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_time_entry
+    @time_entry = TimeEntry.find(params[:id])
+  end
+
+  def time_entry_params
+    params.require(:time_entry).permit(:duration, :project_id, :completed_on, :developer_id)
   end
 end
